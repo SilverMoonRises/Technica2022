67a8d6c00f5fed0d04c17d52422cdf46e262910c
SilverMoonRises <dorafang41@gmail.com>
2022-10-15 21:10:15 -0700

    search bar edit placeholders

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
-// App.js
+import React from 'react';
+import './App.css';
+import Data from './Data.json';
+import searchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className='App'>
+      <searchBar placeholder='' />
+    </div>
+  );
+}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,17 @@
-// code for searchbar
+import React from 'react';
+function searchBar(placeholder, data) {
+  return (
+    <div className='search'>
+      <div className='searchInputs'>
+        <input type='text' placeholder='={}' />
+        <div className='searchIcon'>
+          <div className='dataResult'>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default searchBar;
